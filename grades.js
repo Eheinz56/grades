52b2a21f659ed4ae8c1d17a50a69f63ce2c1ed10
Eric Heinzman <ehu9079@ehu9079-VirtualBox.(none)>
2015-01-26 13:08:01 -0500

    Added comments

--- grades.js
+++ grades.js
@@ -1,3 +1,8 @@
+//Eric Heinzman
+//CMP 344
+//Grades.js
+//adding a function to compute a students average and ability to add grade
+//1/26/15
 
 var grades = [[89, 77, 78],[76, 82, 81,],[91,94,89]];
 function add(array,foo,grade ){
